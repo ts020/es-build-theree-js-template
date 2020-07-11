64247bdc9b5bfb51b5f7cefaf1b57d61933b0fb9
takanori.sugawara <takanori.sugawara@gmail.com>
2020-07-11 15:31:22 +0900

    add three.js template

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
+import { Scene, WebGLRenderer, PerspectiveCamera } from 'three'
 function main() {
-  alert('fefef')
-  console.log('fewfew')
-  console.log('fewfe')
+  const renderer = new WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  const camera = new PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  )
+  camera.position.set(0, 0, 100)
+  camera.lookAt(0, 0, 0)
+
+  const scene = new Scene()
 }
-main()
+window.addEventListener('DOMContentLoaded', main)
